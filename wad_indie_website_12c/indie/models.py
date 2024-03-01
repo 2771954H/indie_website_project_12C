4aba60b4532e2d18486a700950e0dfc1e486c6b8
Tony Soong <2764149s@student.gla.ac.uk>
2024-03-01 15:39:57 +0000

    Create game and genre models

--- wad_indie_website_12c/indie/models.py
+++ wad_indie_website_12c/indie/models.py
@@ -3,36 +3,31 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 
-class Category(models.Model):
-    NAME_MAX_LENGTH = 128
-
-    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
+class Game(models.Model):
+    name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
+    genre = models.ManyToManyField("Genre")
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
-
-    class Meta:
-        verbose_name_plural = "Categories"
+        super(Game, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
 
-class Page(models.Model):
-    TITLE_MAX_LENGTH = 128
-    URL_MAX_LENGTH = 200
+class Genre(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    slug = models.SlugField(unique=True)
 
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=TITLE_MAX_LENGTH)
-    url = models.URLField()
-    views = models.IntegerField(default=0)
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Genre, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.title
+        return self.name
 
 
 class UserProfile(models.Model):
