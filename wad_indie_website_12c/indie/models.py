b548c155f6a281b374013d332268966e15fd438c
Tony Soong <2764149s@student.gla.ac.uk>
2024-03-01 15:47:40 +0000

    Created other models

--- wad_indie_website_12c/indie/models.py
+++ wad_indie_website_12c/indie/models.py
@@ -3,21 +3,6 @@ from django.db import models
 from django.template.defaultfilters import slugify
 
 
-class Game(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-    views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
-    slug = models.SlugField(unique=True)
-    genre = models.ManyToManyField("Genre")
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Game, self).save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
-
-
 class Genre(models.Model):
     name = models.CharField(max_length=128, unique=True)
     slug = models.SlugField(unique=True)
@@ -30,14 +15,38 @@ class Genre(models.Model):
         return self.name
 
 
-class UserProfile(models.Model):
-    # This line is required. Links UserProfile to a User model instance.
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class Game(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    price = models.FloatField(default=0)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    downloads = models.IntegerField(default=0)
 
-    # The additional attributes we wish to include.
-    bio = models.CharField(max_length=9999, blank=True)
-    paypal_address = models.URLField(blank=True)
-    picture = models.ImageField(upload_to="profile_images", blank=True)
+    slug = models.SlugField(unique=True)
+    genre = models.ManyToManyField(Genre)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Game, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Feedback(models.Model):
+    text = models.CharField(max_length=9999, blank=True, null=True)
+    rating = models.IntegerField(blank=True, null=True)
+    user = models.ForeignKey(User)
+    game = models.ForeignKey(Game)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_dev = models.BooleanField(default=False)
+    bio = models.CharField(max_length=9999, blank=True, null=True)
+    paypal_address = models.URLField(blank=True, null=True)
+    picture = models.ImageField(upload_to="profile_images", blank=True, null=True)
+    fav_genre = models.ForeignKey(Genre)
 
     def __str__(self):
         return self.user.username
