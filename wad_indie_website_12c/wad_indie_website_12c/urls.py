24b094950a5e7663b77d2fdc14649f27099834ab
2771954H <2771954H@student.gla.ac.uk>
2024-03-03 13:27:26 +0000

    Updated Settings

--- wad_indie_website_12c/wad_indie_website_12c/urls.py
+++ wad_indie_website_12c/wad_indie_website_12c/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.conf import settings
 from indie import views
 
 urlpatterns = [
