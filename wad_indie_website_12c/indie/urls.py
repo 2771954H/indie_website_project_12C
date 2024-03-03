3f2bc8dc682b925ac6b86decc9de57745f6e0ce1
2771954H <2771954H@student.gla.ac.uk>
2024-03-03 13:47:43 +0000

    Fixed the bugs I created

--- wad_indie_website_12c/indie/urls.py
+++ wad_indie_website_12c/indie/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     
     path('dev_home/', views.dev_home, name = 'dev_home'),
+    
+    path('upload_new_game', views.upload_new_game, name = "upload_new_game"),
+    
+    path('logout', views.logout, name='logout')
 ]
 
--- wad_indie_website_12c/indie/views.py
+++ wad_indie_website_12c/indie/views.py
@@ -8,6 +8,10 @@ def index(request):
     
     return render(request, 'indie/index.html', context= context_dict)
 
+def logout(request):
+        return index(request)
+
+
 
 #Dev pages
 def dev_home(request):
@@ -15,3 +19,12 @@ def dev_home(request):
     context_dict["pagename"] = 'Dev Home'
     
     return render(request, 'indie/dev_home.html', context=context_dict)
+
+def upload_new_game(request):
+    context_dict = {}
+    context_dict["pagename"] = 'Upload New Game'
+    
+    return HttpResponse("Uploading Your Game, Yay!")
+    
+
+
