ad442022c496574e1f86a61d1c56d21fe0a9217b
2771954H <2771954H@student.gla.ac.uk>
2024-03-09 16:26:41 +0000

    External CSS sheet implemented

--- wad_indie_website_12c/wad_indie_website_12c/settings.py
+++ wad_indie_website_12c/wad_indie_website_12c/settings.py
@@ -19,6 +19,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS=[STATIC_DIR,]
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
