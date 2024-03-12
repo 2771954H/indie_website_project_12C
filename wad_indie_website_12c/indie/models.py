79255c8ad021f907a7f73590221d5f874e8a8636
Tony Soong <2764149s@student.gla.ac.uk>
2024-03-12 00:19:53 +0000

    Show game cards on main page

--- wad_indie_website_12c/indie/models.py
+++ wad_indie_website_12c/indie/models.py
@@ -14,12 +14,12 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 
 class UserProfile(models.Model):
     MAX_INPUT_LENGTH = 9999
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, unique = True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     is_dev = models.BooleanField(default=False)
     bio = models.CharField(max_length=MAX_INPUT_LENGTH, blank=True, null=True)
     paypal_address = models.URLField(blank=True, null=True)
@@ -27,15 +27,17 @@ class UserProfile(models.Model):
     fav_genre = models.ForeignKey(
         Genre, on_delete=models.SET_NULL, null=True, blank=True
     )
-    
 
     def __str__(self):
-        return self.user.username   
+        return self.user.username
 
 
 class Game(models.Model):
     NAME_MAX_LENGTH = 128
+    MAX_DESC_LENGTH = 9999
     name = models.CharField(max_length=128, unique=True)
+    description = models.CharField(max_length=MAX_DESC_LENGTH, blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
     price = models.FloatField(default=0)
 
     def price_formatted(self):
@@ -49,9 +51,9 @@ class Game(models.Model):
 
     slug = models.SlugField(unique=True)
     genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True, blank=True)
-    dev = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null =True)
-    
-    #Have not tested this at all- no idea if it actually works
+    dev = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True)
+
+    # Have not tested this at all- no idea if it actually works
     def dev_check(self):
         dev = self._meta.get_fields()[7]
         if not dev._meta.get_fields()[1]:
@@ -70,10 +72,8 @@ class Feedback(models.Model):
 
     text = models.CharField(max_length=MAX_INPUT_LENGTH)
     rating = models.IntegerField()
-    userProfile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null = True)
+    userProfile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return f"{self.userProfile} wrote review for {self.game}. Gave rating of {self.rating}"
-
-
--- wad_indie_website_12c/indie/views.py
+++ wad_indie_website_12c/indie/views.py
@@ -1,30 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from indie.models import Game
+
 
 def index(request):
     context_dict = {}
     context_dict["pagename"] = "Index"
-    
-    return render(request, 'indie/index.html', context= context_dict)
+    games_list = Game.objects.order_by("-likes")[:4]
+    context_dict["games"] = games_list
+    return render(request, "indie/index.html", context=context_dict)
+
 
 def logout(request):
-        return index(request)
+    return index(request)
 
 
-
-#Dev pages
+# Dev pages
 def dev_home(request):
     context_dict = {}
-    context_dict["pagename"] = 'Dev Home'
-    
-    return render(request, 'indie/dev_home.html', context=context_dict)
+    context_dict["pagename"] = "Dev Home"
+
+    return render(request, "indie/dev_home.html", context=context_dict)
+
 
 def upload_new_game(request):
     context_dict = {}
-    context_dict["pagename"] = 'Upload New Game'
-    
+    context_dict["pagename"] = "Upload New Game"
+
     return HttpResponse("Uploading Your Game, Yay!")
-    
-
-
