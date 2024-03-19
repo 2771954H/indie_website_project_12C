f7e98c5874ca18faaf78a23a4a6d356babd76903
Tony Soong <2764149s@student.gla.ac.uk>
2024-03-19 01:14:19 +0000

    Implemented search function, created user pages

--- wad_indie_website_12c/indie/urls.py
+++ wad_indie_website_12c/indie/urls.py
@@ -8,9 +8,11 @@ app_name = "indie"
 urlpatterns = [
     path("", views.index, name="index"),
     path("game/<slug:game_name_slug>/", views.show_game, name="show_game"),
+    path("user/<slug:username>/", views.show_user, name="show_user"),
+    path("search/", views.search, name="search"),
     path("dev_home/", views.dev_home, name="dev_home"),
     path("upload_game/", views.upload_game, name="upload_game"),
     path("logout/", views.logout, name="logout"),
-    path("register", views.register, name = "register"),
-    path('login/', views.user_login, name = 'login')
+    path("register", views.register, name="register"),
+    path("login/", views.user_login, name="login"),
 ]
--- wad_indie_website_12c/indie/views.py
+++ wad_indie_website_12c/indie/views.py
@@ -9,19 +9,32 @@ from indie.forms import GameForm, UserForm, UserProfileForm
 
 def index(request):
     context_dict = {}
-    context_dict["pagename"] = "Index"
-    
+
     featured_games_list = Game.objects.filter(featured=True)[:4]
     popular_games_list = Game.objects.order_by("-downloads")[:4]
     new_games_list = Game.objects.order_by("-upload_date")[:4]
     top_games_list = Game.objects.order_by("-likes")[:4]
-    
+
     context_dict["featured_games"] = featured_games_list
     context_dict["popular_games"] = popular_games_list
     context_dict["new_games"] = new_games_list
     context_dict["top_games"] = top_games_list
-    
+
     return render(request, "indie/index.html", context=context_dict)
+
+
+def search(request):
+    query = request.GET.get("searchtext")
+    context_dict = {}
+
+    games_list = Game.objects.filter(name__contains=query)
+    devs_list = User.objects.filter(username__contains=query, userprofile__is_dev=True)
+
+    context_dict["games"] = games_list
+    context_dict["devs"] = devs_list
+    context_dict["query"] = query
+
+    return render(request, "indie/search.html", context=context_dict)
 
 
 def show_game(request, game_name_slug):
@@ -35,70 +48,85 @@ def show_game(request, game_name_slug):
 
     return render(request, "indie/game_page.html", context=context_dict)
 
+
+def show_user(request, username):
+    context_dict = {}
+
+    try:
+        user = UserProfile.objects.get(user__username=username)
+        context_dict["user"] = user
+    except UserProfile.DoesNotExist:
+        context_dict["user"] = None
+
+    context_dict["username"] = username
+
+    return render(request, "indie/user_page.html", context=context_dict)
+
+
 def register(request):
     registered = False
-    context_dict = {'pagename': 'Register'}
-    
+    context_dict = {"pagename": "Register"}
+
     if request.method == "POST":
         user_form = UserForm(request.POST)
         profile_form = UserProfileForm(request.POST)
-        
+
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
             user.set_password(user.password)
             user.save()
-            
+
             profile = profile_form.save(commit=False)
-            profile.user=user
-            
+            profile.user = user
+
             profile.save()
-            
+
             registered = True
-            context_dict['user'] = user
-            context_dict['profile'] = profile
-            
-    
+            context_dict["user"] = user
+            context_dict["profile"] = profile
+
         else:
             print(user_form.errors, profile_form.errors)
-            
+
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
-        
-    context_dict['pagename'] = 'Register'
-    context_dict['user_form'] = user_form
-    context_dict['profile_form'] = profile_form
-    context_dict['registered'] = registered
-    
-    return render(request, 'indie/register.html', context=context_dict)
+
+    context_dict["pagename"] = "Register"
+    context_dict["user_form"] = user_form
+    context_dict["profile_form"] = profile_form
+    context_dict["registered"] = registered
+
+    return render(request, "indie/register.html", context=context_dict)
+
 
 def user_login(request):
-    context_dict = {'pagename' : 'Login'}
-    
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        
-        user = authenticate(username = username, password = password)
-        profile = UserProfile.objects.get(user = user)
-        
+    context_dict = {"pagename": "Login"}
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(username=username, password=password)
+        profile = UserProfile.objects.get(user=user)
+
         if user:
             if user.is_active:
                 login(request, user)
                 if profile.is_dev:
-                    return redirect(reverse('indie:dev_home'))
+                    return redirect(reverse("indie:dev_home"))
                 else:
-                    return redirect(reverse('indie:index'))
+                    return redirect(reverse("indie:index"))
             else:
-                return HttpResponse('Your Indie account is disabled.')
+                return HttpResponse("Your Indie account is disabled.")
         else:
             print(f"Invalid login details.")
             return HttpResponse("Invalid login details supplied.")
-    
+
     else:
-        return render(request, 'indie/login.html', context=context_dict)
-                
-        
+        return render(request, "indie/login.html", context=context_dict)
+
+
 def logout(request):
     return index(request)
 
@@ -113,18 +141,20 @@ def dev_home(request):
 
 def upload_game(request):
     context_dict = {}
-    context_dict["pagename"] = 'Upload Game'
-    
+    context_dict["pagename"] = "Upload Game"
+
     form = GameForm()
-    
-    if request.method == 'POST':
+
+    if request.method == "POST":
         form = GameForm(request.POST)
-        
+
         if form.is_valid():
-            form.save(commit=True)            
+            form.save(commit=True)
             return redirect("indie/dev_home.html")
-        
+
         else:
             print(form.errors)
-    
-    return render(request, "indie/upload_game.html", {'form' : form}, context=context_dict)
+
+    return render(
+        request, "indie/upload_game.html", {"form": form}, context=context_dict
+    )
