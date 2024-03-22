27ee268bc8ee7d5ed130d0fb70175327ac76f907
nootlynoot <huxiaozu1245@gmail.com>
2024-03-22 22:08:54 +0800

    removed irrelavent stuff

--- wad_indie_website_12c/test_views.py
+++ wad_indie_website_12c/test_views.py
@@ -2,8 +2,6 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 from indie.models import Game, UserProfile
-from django.test.client import RequestFactory
-from indie.helperpythonfunc import get_current_profile
 
 class TestViews(TestCase):
     def setUp(self):
@@ -17,7 +15,7 @@ class TestViews(TestCase):
         response = self.client.get(reverse('indie:index'))
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(response.context['featured_games'], ['<Game: Test Game>'])
-        
+
     def test_show_game_view(self):
         response = self.client.get(reverse('indie:show_game', args=[self.test_game.slug]))
         self.assertEqual(response.status_code, 200)
