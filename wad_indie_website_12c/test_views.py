774e2f8c23115905f84aaa28160adac18016db72
nootlynoot <huxiaozu1245@gmail.com>
2024-03-22 22:08:34 +0800

    test cases

--- wad_indie_website_12c/test_views.py
+++ wad_indie_website_12c/test_views.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 from indie.models import Game, UserProfile
+from django.test.client import RequestFactory
+from indie.helperpythonfunc import get_current_profile
 
 class TestViews(TestCase):
     def setUp(self):
@@ -10,21 +12,14 @@ class TestViews(TestCase):
 
         self.test_game = Game.objects.create(name='Test Game', dev=self.test_user_profile)
 
+    """     this should fail since there are no games inside the website on a fresh boot. """
     def test_index_view(self):
         response = self.client.get(reverse('indie:index'))
         self.assertEqual(response.status_code, 200)
         self.assertQuerysetEqual(response.context['featured_games'], ['<Game: Test Game>'])
-
+        
     def test_show_game_view(self):
         response = self.client.get(reverse('indie:show_game', args=[self.test_game.slug]))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['game'], self.test_game)
-
-    def test_user_login_view(self):
-        response = self.client.post(reverse('indie:user_login'), {'username': 'testuser', 'password': '12345'})
-        self.assertEqual(response.status_code, 302)
-
-        self.test_user_profile.is_dev = True
-        self.test_user_profile.save()
-        response = self.client.post(reverse('indie:user_login'), {'username': 'testuser', 'password': '12345'})
-        self.assertRedirects(response, reverse('indie:dev_home'))
+        
