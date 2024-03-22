ee1613104d7dbd9d9a6788a10a5a71da57877b7b
nootlynoot <huxiaozu1245@gmail.com>
2024-03-22 23:02:05 +0800

    revert

--- wad_indie_website_12c/indie/helperpythonfunc.py
+++ wad_indie_website_12c/indie/helperpythonfunc.py
@@ -1,13 +1,12 @@
 from indie.models import UserProfile
-from indie.views import index
 
 #Helper functions
 #Checks if the current logged in user is a dev- put in every dev page
 def check_dev(request):
     profile = get_current_profile(request)
-    if profile.__getattribute__('is_dev') == False:
+    if profile.is_dev == False:
         return index(request)
     
 
 def get_current_profile(request):
-    return UserProfile.objects.get(user = request.user)
+    profile = UserProfile.object.get(user= request.user)
